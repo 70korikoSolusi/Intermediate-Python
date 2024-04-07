dd854df73c099c9d9eae088a5569cc2f752735bd
70korikoSolusi <166281362+70korikoSolusi@users.noreply.github.com>
2024-04-07 15:40:07 +0700

    Add files via upload

---
+++ Syntak_law.py
@@ -0,0 +1,22 @@
+"""
+Syntak law python
+"""
+print("hello python") # print adalah perintah untuk mencetak ke layar komputer
+# tanda ; adalah pemisah statemen dalam satu baris
+print("hello"); print("apa kabar")
+# , adalah pemisah string yang akan dicetak 
+# statemen multi baris \n adalah format string untuk pindah baris \t adalah format tab
+print('-'*15,\
+ "\nnama\temail") 
+print('-'*15) # membuat garis terputus dimana '-' diulang 15 kali
+"""
+Tugas Syntak law buat tampilan menu makanan
+-------------------------------------- 
+         menu makanan 
+-------------------------------------- 
+ 1. Es Teh + Nasi Goreng
+ 2. Puncuk + Bakso
+ 3. Es Teh + Mie Ayam Bakso
+---------------------------------------
+"""
+
